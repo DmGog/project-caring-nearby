0a4b9260a289b9eee7350b25810793d106f369ba
Dmitriy <goglev37@gmail.com>
2025-01-17 14:57:09 +0300

    fix: fix pagination

--- src/features/my-profile/ui/favorites/favorites.tsx
+++ src/features/my-profile/ui/favorites/favorites.tsx
@@ -2,7 +2,9 @@ import {
     CardMap,
     HelpRequest,
     HelpRequests,
-    useHelpRequestsQuery, useUserHelpFavoritesRequestsQuery,
+    ProfilePageSkeleton,
+    useHelpRequestsQuery,
+    useUserHelpFavoritesRequestsQuery,
 } from "@/features";
 import {AlignmentType, NotFoundResult, ToggleButtonsGroup} from "@/shared";
 import {CardsListItemRequest, CardsRequest} from "@/widgets";
@@ -14,11 +16,20 @@ import {useSearchParams} from "react-router-dom";
 export const Favorites = () => {
     const {data: helpRequests} = useHelpRequestsQuery();
     const {data: favoritesHelps, isLoading} = useUserHelpFavoritesRequestsQuery();
+
     const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 3;
+
+
+    const totalPages = Math.ceil((favoritesHelps?.length ?? 0) / itemsPerPage);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
     const [searchParams, setSearchParams] = useSearchParams();
     const initialAlignment = (searchParams.get("view") as AlignmentType) || "left";
     const [alignment, setAlignment] = useState<AlignmentType>(initialAlignment);
+
     const handleAlignmentChange = (newAlignment: AlignmentType) => {
         setAlignment(newAlignment);
         setSearchParams(params => {
@@ -34,7 +45,14 @@ export const Favorites = () => {
                 return params;
             });
         }
-    })
+    }, [searchParams, setSearchParams]);
+
+    useEffect(() => {
+        if (currentPage > totalPages) {
+            setCurrentPage(1);
+        }
+    }, [totalPages, currentPage]);
+
 
     if (!helpRequests) {
         return <NotFoundResult title={"Ошибка! Не удалось загрузить запросы"} img={"infoNotImage"} color={"red"}/>;
@@ -48,15 +66,12 @@ export const Favorites = () => {
         )
         : [];
 
-    const itemsPerPage = 3;
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = favoriteRequests.slice(indexOfFirstItem, indexOfLastItem);
-    const handlePageChange = (value: number) => {
-        setCurrentPage(value);
-    };
 
     if (!favoritesHelps) return null;
+    if (isLoading) {
+        return <ProfilePageSkeleton/>
+    }
 
     return (
         <Box
@@ -69,7 +84,7 @@ export const Favorites = () => {
                 minHeight: "973px",
                 justifyContent: "space-between"
             }}>
-            <Box  position="absolute" top="-40px" right="0px">
+            <Box position="absolute" top="-40px" right="0px">
                 <ToggleButtonsGroup alignment={alignment} onAlignmentChange={handleAlignmentChange}/>
             </Box>
             {!favoritesHelps &&
@@ -79,13 +94,12 @@ export const Favorites = () => {
             {alignment === "center" && <CardsListItemRequest data={currentItems} favoriteHelps={favoritesHelps}/>}
             {alignment === "right" && <CardMap/>}
             <Pagination
-                disabled={isLoading}
                 sx={{
                     pt: "30px"
                 }}
-                count={Math.ceil((favoriteRequests.length) / itemsPerPage)}
+                count={totalPages}
                 page={currentPage}
-                onChange={(_, value) => handlePageChange(value)}
+                onChange={(_, value) => setCurrentPage(value)}
                 color="primary"
             />
         </Box>
